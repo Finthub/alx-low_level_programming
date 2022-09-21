9a630658c874450f7fd3d91b11bb77f408d46e16
Finthub <gracemania74@gmail.com>
2022-09-21 14:15:00 +0000

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr[];
+	int i,j,k ;
+
+	printf("%s\n", s1);
+	printf("%s\n", s2);
+    for (i=0, i<98, i++)
+{   for (k=0, k<98, k++)
+  {
+	ptr[k]=s1[i];
+  }    
+} 
+	for (j=0, s1[j] !='\0', j++)
+{       for (k=98, k<(strlen(s1)+strlen(s2)), k++)
+  {
+        ptr[k]=s1[j];
+  } 
+} 
+	printf("%s", s1);
+        printf("%s", s2);
+        printf("%s", ptr);
+        return (0);
+}
+
